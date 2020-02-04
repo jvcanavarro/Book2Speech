418da22d5dfc4df330eda838e41da761837bc46e
João Canavarro <jvcanavarro@gmail.com>
2020-02-04 12:29:06 -0300

    Added samples!

--- source/image_loader.py
+++ source/image_loader.py
@@ -3,17 +3,18 @@ import cv2
 
 def capture_image():
     webcam = cv2.VideoCapture(0)
+    image = []
     while True:
         flag, frame = webcam.read()
         cv2.imshow('Rasp3 Camera', frame)
 
-        image = []
         key = cv2.waitKey(1)
 
         if key == ord('s'):
-            cv2.imwrite(filename='image.jpg', img=frame)
-            image = cv2.imread('image.jpg')
-            image = cv2.imshow('Captured Image', image)
+            # cv2.imwrite(filename='image.jpg', img=frame)
+            # image = cv2.imread('image.jpg')
+            image = frame
+            cv2.imshow('Captured Image', image)
             cv2.waitKey(500)
             cv2.destroyAllWindows()
 
@@ -21,7 +22,6 @@ def capture_image():
             webcam.release()
             cv2.destroyAllWindows()
             return image
-            # return 'image.jpg'
 
 
 def load_image(filename):
--- source/main.py
+++ source/main.py
@@ -1,1 +1,25 @@
 from image_loader import capture_image, load_image, load_folder
+from glob import glob
+import image_loader
+import click
+
+
+@click.command()
+@click.option('-i', '--image', type=click.Path(exists=True))
+@click.option('-p', '--path', type=click.Path(exists=True))
+def start(image, path):
+    if image:
+        pic = load_image(image)
+    elif path:
+        files = glob(path + '*jpg') + glob(path + '*png')
+        pic = load_folder(files)
+    else:
+        pic = capture_image()
+
+    print(len(pic))
+
+
+
+
+if __name__ == '__main__':
+    start()
