939847b499aed7b5b70e0ebd59fab9e95dc80f88
João Canavarro! <jvcanavarro@gmail.com>
2021-03-24 18:09:09 -0300

    Remove Book2Speech I files

--- source/tts.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-
-def get_audio(filename):
-    text = open(filename, 'rb').read()
-    output = filename[:-3] + '.wav'
-    tts = gTTS(text, lang='pt')
-    tts.save(output)
