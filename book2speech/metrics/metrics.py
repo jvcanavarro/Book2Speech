283ee13220bc46854e66c0871ef9ade888e0ee1e
João Canavarro <jvcanavarro@gmail.com>
2021-03-31 20:05:26 -0300

    Remove transformations from `metrics`

--- book2speech/metrics/metrics.py
+++ book2speech/metrics/metrics.py
@@ -1,32 +1,11 @@
 import jiwer
 
-def calculate_metrics(ground_truth, hypothesis, extend_transformations=False):
-    if extend_transformations:
-        transformation = jiwer.Compose([
-        jiwer.ToLowerCase(),
-        jiwer.RemoveWhiteSpace(replace_by_space=True),
-        jiwer.RemoveMultipleSpaces(),
-        jiwer.Strip(),
-        jiwer.SentencesToListOfWords(),
-        jiwer.RemoveEmptyStrings()
-        ])
-    else:
-        transformation = jiwer.Compose([
-        jiwer.RemoveMultipleSpaces(),
-        jiwer.Strip(),
-        jiwer.SentencesToListOfWords(),
-        jiwer.RemoveEmptyStrings()
-        ])
 
-    metrics = jiwer.compute_measures(
-        ground_truth, 
-        hypothesis, 
-        truth_transform=transformation, 
-        hypothesis_transform=transformation
-    )
+def generate_metrics(ground_truth, hypothesis):
+    metrics = jiwer.compute_measures(ground_truth, hypothesis)
 
     print('WER: {:.2f}%'.format(metrics['wer'] * 100))
-    print('MER {:.2f}%'.format(metrics['mer'] * 100))
+    print('CER: {:.2f}%'.format(metrics['mer'] * 100))
     print('WIL: {:.2f}%'.format(metrics['wil'] * 100))
     print('WIP: {:.2f}%'.format(metrics['wip'] * 100))
     print('-' * 15)
@@ -34,5 +13,4 @@ def calculate_metrics(ground_truth, hypothesis, extend_transformations=False):
     print('Substitutions: {}'.format(metrics['substitutions']))
     print('Deletions: {}'.format(metrics['deletions']))
     print('Insertions: {}'.format(metrics['insertions']))
-    return metrics 
-
+    return metrics
---
+++ book2speech/transformations/transformations.py
@@ -0,0 +1,22 @@
+import jiwer
+
+
+def apply_transformations(ground_truth, hypothesis, transformation_type):
+    # TODO: select a list of transformations
+    transformation = jiwer.Compose([
+        jiwer.RemoveMultipleSpaces(),
+        jiwer.Strip(),
+        jiwer.RemoveEmptyStrings(),
+        jiwer.SentencesToListOfWords()
+    ])
+    if transformation_type == 'reduced':
+        transformation = transformation.transforms[-1]
+
+    if transformation_type == 'extended':
+        transformation.transforms = [
+            jiwer.ToLowerCase(),
+            jiwer.RemoveWhiteSpace(replace_by_space=True),
+            jiwer.RemovePunctuation()
+        ] + transformation.transforms
+
+    return transformation(ground_truth), transformation(hypothesis) 
