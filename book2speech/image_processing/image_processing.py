d34728ff69f207feb97197dea0d588817a348f2e
João Canavarro <jvcanavarro@gmail.com>
2021-04-14 21:15:03 -0300

    Add dewarp and remove deskew from image processing

--- book2speech/image_processing/image_processing.py
+++ book2speech/image_processing/image_processing.py
@@ -1,27 +1,18 @@
 import numpy as np
 import cv2
 
-
-def deskew(thresh):
-    h, w = thresh.shape[:2]
-    coords = np.column_stack(np.where(thresh > 0))
-    center = (w // 2, h // 2)
-
-    angle = cv2.minAreaRect(coords)[-1]
-    angle = -(90 + angle) if angle < -45 else -angle
-
-    matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated_image = cv2.warpAffine(
-        thresh, matrix, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE
-    )
-
-    return rotated_image
+from image_processing.dewarp import dewarp_image
 
 
 def improve_image_quality(
-    image_path, blur_mode, thresh_mode, deskew_image, output, save
+    image_path, blur_mode, thresh_mode, dewarp, optimizer, output, save
 ):
-    image = cv2.imread(image_path, 0)
+
+    image = (
+        dewarp_image(image_path, optimizer, save)
+        if dewarp
+        else cv2.imread(image_path, 0)
+    )
 
     if blur_mode == "average":
         image = cv2.blur(image, (5, 5))
@@ -33,18 +24,22 @@ def improve_image_quality(
         image = cv.bilateralFilter(image, 9, 75, 75)
 
     if thresh_mode == "simple":
-        image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
-    elif thresh_mode == "adaptative":
+        image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)[1]
+    elif thresh_mode == "gaussian":
         image = cv2.adaptiveThreshold(
             image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2
         )
+    elif thresh_mode == "mean":
+        image = cv2.adaptiveThreshold(
+            image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 2
+        )
     elif thresh_mode == "otsu":
-        image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-
-    if deskew_image:
-        image = deskew(image)
+        image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
     if save:
         cv2.imwrite(output + ".png", image)
+
+    # cv2.imshow("image", image)
+    # cv2.waitKey(0)
 
     return image
