a83548490961010179b09f0ebc37ba1ae731af8d
João Canavarro <jvcanavarro@gmail.com>
2021-04-14 21:14:39 -0300

    Remove tesserocr

--- book2speech/ocr/ocr.py
+++ book2speech/ocr/ocr.py
@@ -1,17 +1,6 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
 import pytesseract
-from tesserocr import PyTessBaseAPI
 
 
-def image_to_text(image, lang, use_tesserocr):
-    if use_tesserocr:
-        with PyTessBaseAPI(lang=lang) as api:
-            api.SetImageFile(image)
-            text = api.GetUTF8Text()
-    else:
-        config = "-c tessedit_do_invert=0"
-        text = pytesseract.image_to_string(image, lang=lang, config=config)
-    return text
+def image_to_text(image, lang):
+    config = "-c tessedit_do_invert=0"
+    return pytesseract.image_to_string(image, lang=lang, config=config)
