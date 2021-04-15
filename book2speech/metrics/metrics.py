fbb1b755ae9d97ceb45d06825bf03f9b4baeb968
João Canavarro <jvcanavarro@gmail.com>
2021-04-14 21:14:47 -0300

    Improve metrics readability

--- book2speech/metrics/metrics.py
+++ book2speech/metrics/metrics.py
@@ -5,6 +5,7 @@ def generate_metrics(ground_truth, hypothesis, verbose):
     metrics = jiwer.compute_measures(ground_truth, hypothesis)
 
     if verbose:
+        print()
         print("WER: {:.2f}%".format(metrics["wer"] * 100))
         print("CER: {:.2f}%".format(metrics["mer"] * 100))
         print("WIL: {:.2f}%".format(metrics["wil"] * 100))
@@ -14,5 +15,10 @@ def generate_metrics(ground_truth, hypothesis, verbose):
         print("Substitutions: {}".format(metrics["substitutions"]))
         print("Deletions: {}".format(metrics["deletions"]))
         print("Insertions: {}".format(metrics["insertions"]))
+        print("-" * 15)
+        print("Truth: {}".format(metrics["truth"]))
+        print()
+        print("Hypothesis: {}".format(metrics["hypothesis"]))
+        
 
     return metrics
