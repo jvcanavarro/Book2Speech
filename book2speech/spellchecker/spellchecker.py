a89554c29e5e568006c92ccbe36e426b90d2a0ac
João Canavarro <jvcanavarro@gmail.com>
2021-04-14 21:14:29 -0300

    Refactor spellchecker

--- book2speech/spellchecker/spellchecker.py
+++ book2speech/spellchecker/spellchecker.py
@@ -2,12 +2,12 @@ from itertools import islice
 from symspellpy import SymSpell, Verbosity
 
 
-def correct_spelling(text, dictionary_path, mode):
+def correct_spelling(text, dictionary_path, bigram_path, mode):
     sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+    sym_spell.load_dictionary(dictionary_path, 0, 1)
 
     correct_text = []
     if mode == "simple":
-        sym_spell.load_dictionary(dictionary_path, 0, 1)
         suggestions = sym_spell.lookup(
             " ".join(text),
             verbosity=Verbosity.TOP,
@@ -16,14 +16,13 @@ def correct_spelling(text, dictionary_path, mode):
         )
         correct_text = suggestions[0].term.split()
 
+    elif mode == "segmentation":
+        suggestions = sym_spell.word_segmentation(" ".join(text), max_edit_distance=2)
+        correct_text = suggestions[0].split()
+
     elif mode == "compound":
         sym_spell.load_bigram_dictionary(dictionary_path, 0, 1)
         suggestions = sym_spell.lookup_compound(" ".join(text), max_edit_distance=2)
         correct_text = suggestions[0].term.split()
-
-    elif mode == "segmentation":
-        sym_spell.load_dictionary(dictionary_path, 0, 1)
-        suggestions = sym_spell.word_segmentation(" ".join(text), max_edit_distance=2)
-        correct_text = suggestions[0].split()
 
     return correct_text
