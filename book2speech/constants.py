6ad94922b87cc19d8f93b230afd5e5b2cc9a1b05
João Canavarro <jvcanavarro@gmail.com>
2021-04-15 21:23:43 -0300

    Extract constants from core

---
+++ book2speech/constants.py
@@ -0,0 +1,23 @@
+TRANSFORMATIONS = ["reduced", "default", "extended"]
+CORRECTIONS = ["simple", "compound", "segmentation"]
+
+THRESHS = ["simple", "gaussian", "mean", "otsu", "disable"]
+BLURS = ["average", "gaussian", "median", "bilateral", "disable"]
+
+OPTIMIZERS = [
+    "Nelder-Mead",
+    "Powell",
+    "CG",
+    "BFGS",
+    "Newton-CG",
+    "L-BFGS-B",
+    "TNC",
+    "COBYLA",
+    "SLSQP",
+    "trust-constr",
+    "dogleg",
+    "trust-ncg",
+    "trust-exact",
+    "trust-krylov",
+    "disable",
+]
