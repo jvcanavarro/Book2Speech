8dcbe6d5a8f5262cfd50a2480fd612022143d01d
João Canavarro <jvcanavarro@gmail.com>
2021-04-18 23:21:59 -0300

    Fix confusion matrix wrong edit index

--- book2speech/plot/confusion_matrix.py
+++ book2speech/plot/confusion_matrix.py
@@ -10,40 +10,31 @@ def generate_confusion_matrix(metrics, verbose):
     truth = metrics["truth"]
     hypothesis = metrics["hypothesis"]
 
-    # print(truth)
-    # print()
-    # print(hypothesis)
-    aux = list(truth)
+    aux = list(hypothesis)
 
     true = []
     pred = []
 
     for edit in metrics["editops"]:
-        print(edit)
         if edit[0] == "insert":
-            print(f"Insert {truth[edit[1]]}")
-            true.append(truth[edit[1]])
+            true.append(truth[edit[2]])
             pred.append("_")
 
         elif edit[0] == "replace":
-            print(f"Replace {hypothesis[edit[2]]}")
-            print(f"with {truth[edit[1]]}")
 
-            true.append(truth[edit[1]])
-            pred.append(hypothesis[edit[2]])
+            true.append(truth[edit[2]])
+            pred.append(hypothesis[edit[1]])
             aux.pop(edit[2])
 
         elif edit[0] == "delete":
-            print(f"Delete {hypothesis[edit[2]]}")
-
             true.append("_")
-            pred.append(hypothesis[edit[2]])
+            pred.append(hypothesis[edit[1]])
             aux.pop(edit[2])
 
     true += aux
     pred += aux
 
-    if not verbose:
+    if verbose verbose:
         print(metrics["editops"])
         print()
         print("".join(true))
@@ -52,8 +43,8 @@ def generate_confusion_matrix(metrics, verbose):
     index = sorted(set(truth + hypothesis + ""))
     cmatrix = confusion_matrix(true, pred, normalize="pred")
 
-    # df = pd.DataFrame(cmatrix, index=index, columns=index)
+    df = pd.DataFrame(cmatrix, index=index, columns=index)
 
-    # plt.figure(figsize=(10, 10))
-    # sn.heatmap(df) 
-    # plt.show()
+    plt.figure(figsize=(10, 10))
+    sn.heatmap(df) 
+    plt.show()
