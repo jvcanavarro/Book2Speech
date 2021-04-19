9619a6d7c883567200361dc0cf6798ef48a89350
João Canavarro <jvcanavarro@gmail.com>
2021-04-18 22:56:05 -0300

    Debug wrong confusion matrix

--- book2speech/plot/confusion_matrix.py
+++ book2speech/plot/confusion_matrix.py
@@ -9,22 +9,33 @@ from sklearn.metrics import confusion_matrix
 def generate_confusion_matrix(metrics, verbose):
     truth = metrics["truth"]
     hypothesis = metrics["hypothesis"]
+
+    # print(truth)
+    # print()
+    # print(hypothesis)
     aux = list(truth)
 
     true = []
     pred = []
 
     for edit in metrics["editops"]:
+        print(edit)
         if edit[0] == "insert":
+            print(f"Insert {truth[edit[1]]}")
             true.append(truth[edit[1]])
             pred.append("_")
 
         elif edit[0] == "replace":
+            print(f"Replace {hypothesis[edit[2]]}")
+            print(f"with {truth[edit[1]]}")
+
             true.append(truth[edit[1]])
             pred.append(hypothesis[edit[2]])
             aux.pop(edit[2])
 
         elif edit[0] == "delete":
+            print(f"Delete {hypothesis[edit[2]]}")
+
             true.append("_")
             pred.append(hypothesis[edit[2]])
             aux.pop(edit[2])
@@ -38,10 +49,11 @@ def generate_confusion_matrix(metrics, verbose):
         print("".join(true))
         print("".join(pred))
 
-    cmatrix = confusion_matrix(true, pred, normalize='all')
-    print("\n", cmatrix)
-    index = sorted(set(truth + hypothesis + "_"))
-    df_cm = pd.DataFrame(cmatrix, index=index, columns=index)
-    plt.figure(figsize=(10, 7))
-    sn.heatmap(df_cm, annot=True)
-    plt.show()
+    index = sorted(set(truth + hypothesis + ""))
+    cmatrix = confusion_matrix(true, pred, normalize="pred")
+
+    # df = pd.DataFrame(cmatrix, index=index, columns=index)
+
+    # plt.figure(figsize=(10, 10))
+    # sn.heatmap(df) 
+    # plt.show()
