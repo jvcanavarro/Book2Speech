05f3631e5090a9caa26891929cd1843d46477f3c
João Canavarro <jvcanavarro@gmail.com>
2021-04-28 18:27:21 -0300

    Add boundbox draw to ocr

--- book2speech/ocr/ocr.py
+++ book2speech/ocr/ocr.py
@@ -1,6 +1,25 @@
+import cv2
 import pytesseract
+from pytesseract import Output
+
+CONFIG = "-c tessedit_do_invert=0"
 
 
-def image_to_text(image, lang):
-    config = "-c tessedit_do_invert=0"
-    return pytesseract.image_to_string(image, lang=lang, config=config)
+def image_to_text(image, lang, bbox):
+    if type(image) == str:
+        image = cv2.imread(image)
+
+    if bbox:
+        data = pytesseract.image_to_data(image, output_type=Output.DICT)
+        n_boxes = len(data["level"])
+        for i in range(n_boxes):
+            x, y, w, h = (
+                data["left"][i],
+                data["top"][i],
+                data["width"][i],
+                data["height"][i],
+            )
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imwrite("output_bb.png", image)
+
+    return pytesseract.image_to_string(image, lang=lang, config=CONFIG)
