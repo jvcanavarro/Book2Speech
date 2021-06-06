1226f0d3598301cb20804048ba9733cd6f8253fc
João Canavarro <jvcanavarro@gmail.com>
2021-06-05 22:32:58 -0300

    Fix confusion matrix in border cases

--- book2speech/plot/confusion_matrix.py
+++ book2speech/plot/confusion_matrix.py
@@ -8,8 +8,7 @@ from sklearn.metrics import confusion_matrix
 
 def plot_matrix(cmatrix, index):
     df = pd.DataFrame(cmatrix, index=index, columns=index)
-
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(20, 20))
     sn.heatmap(df, cmap="Pastel1_r", linewidths=0.5)
     plt.show()
 
@@ -21,6 +20,7 @@ def generate_confusion_matrix(metrics, verbose):
     aux = list(hypothesis)
     true = []
     pred = []
+    temp = 0
 
     for edit in metrics["editops"]:
         if edit[0] == "insert":
@@ -30,12 +30,14 @@ def generate_confusion_matrix(metrics, verbose):
         elif edit[0] == "replace":
             true.append(truth[edit[2]])
             pred.append(hypothesis[edit[1]])
-            aux.pop(edit[2])
+            aux.pop(edit[2] - temp)
+            temp += 1
 
         elif edit[0] == "delete":
             true.append("_")
             pred.append(hypothesis[edit[1]])
-            aux.pop(edit[2])
+            aux.pop(edit[2] - temp)
+            temp += 1
 
     if verbose:
         print(f"True: {''.join(true)}")
@@ -44,8 +46,10 @@ def generate_confusion_matrix(metrics, verbose):
     true += aux
     pred += aux
 
-    cmatrix = confusion_matrix(true, pred, normalize="pred")
-    index = sorted(set(truth + hypothesis + "_"))
+    cmatrix = confusion_matrix(true, pred)
+
+    ids = set(truth + hypothesis + "_")
+    index = sorted(ids if len(metrics["editops"]) > 0 else ids[:-1])
 
     plot_matrix(cmatrix, index)
 
